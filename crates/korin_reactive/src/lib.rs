aa8ed36e5bfa6d32ad149693184772f788069189
MSILycanthropy <ethanmichaelk@gmail.com>
2025-12-04 15:27:03 -0600

    feat: export `reactive_graph` functions

---
+++ crates/korin_reactive/src/lib.rs
@@ -0,0 +1,24 @@
+use reactive_graph::owner::LocalStorage;
+
+pub use reactive_graph::{
+    computed::Memo,
+    effect::Effect,
+    signal::{ReadSignal, RwSignal, WriteSignal, signal},
+};
+
+pub fn rw_signal<T: Send + Sync + 'static>(val: T) -> RwSignal<T> {
+    RwSignal::new(val)
+}
+
+pub fn memo<T>(f: impl Fn(Option<&T>) -> T + Send + Sync + 'static) -> Memo<T>
+where
+    T: Send + Sync + Clone + PartialEq + 'static,
+{
+    Memo::new(f)
+}
+
+pub fn effect<T: 'static>(
+    f: impl FnMut(Option<T>) -> T + Send + Sync + 'static,
+) -> Effect<LocalStorage> {
+    Effect::new(f)
+}
